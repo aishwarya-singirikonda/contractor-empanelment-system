65b234eb37b1fea45a8889d66576c6d9db597065
Aishwarya Singirikonda <sr8005aishwarya@gmail.com>
2026-06-16 09:07:18 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://contractor-backend-9qa0.onrender.com",
 });
 
 export default api;
